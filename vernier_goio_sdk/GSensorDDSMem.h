03cc8075cae43d86506ac73312dd194218e972ac
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-09-12 22:22:43 -0400

    updates from v2.28 of the Vernier GoIO SDK
    see: http://github.com/concord-consortium/goio_sdk/tree/xcode_3_2_macosx_10_6

--- vernier_goio_sdk/GSensorDDSMem.h
+++ vernier_goio_sdk/GSensorDDSMem.h
@@ -4,8 +4,8 @@
 //
 // Declare the structure of the memory record stored on a smart sensor.
 //
-// The fields in the structure that are declared as shorts and floats are stored on the sensor in little endian 
-// format. These same fields are typically manipulated on a host computer in platform specific endian format, 
+// The fields in the structure that are declared as shorts and floats are stored on the sensor in little endian
+// format. These same fields are typically manipulated on a host computer in platform specific endian format,
 // eg. little endian for Wintel machines, and big endian for Macs, so marshalling is performed.
 //
 // Note that we are also assuming that the platform specific implementaton of float is 4 bytes long(we ASSERT this)
@@ -18,9 +18,11 @@ typedef enum
 	kSensorIdNumber_Voltage10 = 2,
 	kSensorIdNumber_FirstSmartSensor = 20,
    	kSensorIdNumber_GoTemp  = 60 ,
-   	kSensorIdNumber_GoMotion  = 69 
+   	kSensorIdNumber_GoMotion  = 69
 } ESensorIdNumber;//This must be consistent with SensorMap.xml.
 
+#if !defined VST_EEQUATIONTYPE // To avoid multiply defined problems GoIO/NGIO/LoggerPro
+#define VST_EEQUATIONTYPE
 typedef enum
 {
 	kEquationType_None = 0,
@@ -41,46 +43,66 @@ typedef enum
 	kEquationType_HeatPulser = 15,
 	kEquationType_DropCounter = 16
 } EEquationType;
+#endif
 
-typedef enum 
-{ 
-//	kProbeTypeNoProbe = 0,
-//	kProbeTypeTime,
+#if !defined VST_EPROBETYPE // To avoid multiply defined problems between GoIO/NGIO/LoggerPro 
+#define VST_EPROBETYPE
+typedef enum
+{
+	kProbeTypeNoProbe = 0,
+	kProbeTypeTime,
 	kProbeTypeAnalog5V = 2, //Skip supports this!
 	kProbeTypeAnalog10V = 3,//Skip supports this!
-//	kProbeTypeHeatPulser,
-//	kProbeTypeAnalogOut,
+	kProbeTypeHeatPulser,
+	kProbeTypeAnalogOut,
 	kProbeTypeMD = 6,
-//	kProbeTypePhotoGate = 7,
+	kProbeTypePhotoGate = 7,
 //	kProbeTypePhotoGatePulseWidth,  no longer allowed (never used)
 //	kProbeTypePhotoGatePulsePeriod, no longer allowed (never used)
-//	kProbeTypeDigitalCount = 10,
-//	kProbeTypeRotary,
-//	kProbeTypeDigitalOut,
-//	kNumProbeTypes
+	kProbeTypeDigitalCount = 10,
+	kProbeTypeRotary,
+	kProbeTypeDigitalOut,
+	kProbeTypeLabquestAudio,
+	kNumProbeTypes
 } EProbeType;
+#endif
 
-#ifdef TARGET_OS_WIN
+#if defined (TARGET_OS_WIN)
 #pragma pack(push)
 #pragma pack(1)
+#endif
+
+#ifdef TARGET_OS_MAC
+#pragma pack(1)
+#endif
+
+//This is unfortunate, but gcc 3.x does not support pragma pack(gcc 4.x does!).
+//We are stuck with gcc 3.x for now, so we use _XPACK1 .
+//Note that some docs on the web mentioned problems with using typedefs and
+//__attribute__, so we are putting the typedef on a separate line.
+#ifndef _XPACK1
+#ifdef TARGET_OS_LINUX
+#define _XPACK1 __attribute__((__packed__))
 #else
-#pragma options align=packed
+#define _XPACK1
+#endif
 #endif
 
 #define MAX_CALIBRATION_UNITS_CHARS_ON_SENSOR 7
 
-typedef struct 
+struct tagGCalibrationPage
 {
 	float		CalibrationCoefficientA;
 	float		CalibrationCoefficientB;
 	float		CalibrationCoefficientC;
 	char		Units[MAX_CALIBRATION_UNITS_CHARS_ON_SENSOR];
-} GCalibrationPage;
+} _XPACK1;
+typedef struct tagGCalibrationPage GCalibrationPage;
 
-typedef struct 
+struct tagGSensorDDSRec
 {
 	unsigned char	MemMapVersion;
-	unsigned char	SensorNumber;			//Identifies type of sensor; (SensorNumber >= 20) generally implies that 
+	unsigned char	SensorNumber;			//Identifies type of sensor; (SensorNumber >= 20) generally implies that
 											//GSensorDDSRec is stored on the sensor hardware. Such sensors are called 'smart'.
 	unsigned char	SensorSerialNumber[3];	//[0][1][2] - serial number as 3-byte integer, Little-Endian (LSB first).
 	unsigned char	SensorLotCode[2];		//Lot code as 2-byte BCD date, [0] = YY, [1] == WW.
@@ -96,7 +118,7 @@ typedef struct
 	unsigned short	TypNumberofSamples;
 	unsigned short	WarmUpTime;				//Time (in seconds) required for the sensor to have power before reaching equilibrium.
 	unsigned char	ExperimentType;
-	unsigned char	OperationType;			//This is a LabPro specific field. 
+	unsigned char	OperationType;			//This is a LabPro specific field.
 											//Go! devices use this field to infer probe type(5 volt or 10 volt). See EProbeType.
 	char			CalibrationEquation;	//See EEquationType.
 	float			YminValue;
@@ -106,13 +128,16 @@ typedef struct
 	unsigned char	ActiveCalPage;
 	GCalibrationPage	CalibrationPage[3];
 	unsigned char	Checksum;				//Result of XORing bytes 0-126.
-} GSensorDDSRec;
+} _XPACK1;
+typedef struct tagGSensorDDSRec GSensorDDSRec;
 
-#ifdef TARGET_OS_WIN
+#if defined (TARGET_OS_WIN)
 #pragma pack(pop)
-#else
-#pragma options align=reset
 #endif
 
+#ifdef TARGET_OS_MAC
+#pragma pack()
+#endif
 
-#endif // _GSENSORDDSMEM_H_
+#endif
+
